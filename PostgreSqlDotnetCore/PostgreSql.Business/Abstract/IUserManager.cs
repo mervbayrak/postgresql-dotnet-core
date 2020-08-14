6ef25e72521e1d63f6364e1686d2369d57cb70d3
Merve <mervbayrak@hotmail.com>
2020-08-14 11:44:46 +0300

    Added User Manager

---
+++ PostgreSqlDotnetCore/PostgreSql.Business/Abstract/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using PostgreSql.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgreSql.Business.Abstract
+{
+    public interface IUserManager
+    {
+        void Add(Users u);
+        void Update(Users u);
+        void Delete(Users u);
+        Users GetById();
+        List<Users> GetList();
+    }
+}
---
+++ PostgreSqlDotnetCore/PostgreSql.Business/Concrete/UserManager.cs
@@ -0,0 +1,41 @@
+﻿using PostgreSql.Business.Abstract;
+using PostgreSql.DAL.Abstract;
+using PostgreSql.DAL.Concrete.EntityFramework.Management;
+using PostgreSql.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgreSql.Business.Concrete
+{
+    public class UserManager : IUserManager
+    {
+        private IUserDAL _userDAL;
+        public UserManager() : this(new EFUserDAL()) { }
+
+        public UserManager(EFUserDAL userDAL)
+        {
+            _userDAL = userDAL;
+        }
+        public void Add(Users u)
+        {
+            _userDAL.Add(u);
+        }
+        public void Update(Users u)
+        {
+            _userDAL.Update(u);
+        }
+        public void Delete(Users u)
+        {
+            _userDAL.Delete(u);
+        }
+        public Users GetById()
+        {
+            return _userDAL.Get();
+        }
+        public List<Users> GetList()
+        {
+            return _userDAL.GetList();
+        }
+    }
+}
